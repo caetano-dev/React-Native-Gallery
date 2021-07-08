3c057b8de31ca945fa8ad10c2977afb93f75c125
drull1000 <pcaetanop@protonmail.com>
2021-07-08 11:47:10 -0300

    Array of images

---
+++ src/data/Api.js
@@ -0,0 +1,37 @@
+export const api = [
+  {
+    title: "Bom Dia",
+    link: "https://picsum.photos/500",
+    id: 0,
+  },
+  {
+    title: "Good Morning",
+    link: "https://picsum.photos/600",
+    id: 1,
+  },
+  {
+    title: "Bonjour",
+    link: "https://picsum.photos/670",
+    id: 2,
+  },
+  {
+    title: "Buenos Dias",
+    link: "https://picsum.photos/300",
+    id: 3,
+  },
+  {
+    title: "Buongiorno",
+    link: "https://picsum.photos/340",
+    id: 4,
+  },
+  {
+    title: "Gutten Tag",
+    link: "https://picsum.photos/250",
+    id: 5,
+  },
+  {
+    title: "Goedendag",
+    link: "https://picsum.photos/700",
+    id: 6,
+  },
+];
