093499fdd48612910ed8df2b85f4644ed54d82dd
drull1000 <pcaetanop@protonmail.com>
2021-07-08 11:46:42 -0300

    Adding styles and components

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
+import { ImageList } from "./src/components/ImageList.js";
+import { Header } from "./src/components/Header.js";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Header />
+      <ImageList />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 70,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
