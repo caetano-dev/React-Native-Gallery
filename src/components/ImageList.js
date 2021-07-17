905a58028aa5d3b4befdb2a0f7d5e35db8aa7ecd
drull1000 <pcaetanop@protonmail.com>
2021-07-17 11:31:49 -0300

    Removing scrollview

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,22 +1,20 @@
 import React from 'react';
-import {
-  StyleSheet,
-  ScrollView,
-  SafeAreaView,
-  FlatList,
-} from 'react-native';
+import { SafeAreaView, FlatList, View, StyleSheet } from 'react-native';
 import { api } from '../data/Api';
 import { ImageItem } from './ImageItem';
 
 export function ImageList() {
-
   const renderItem = ({ item }) => <ImageItem title={item.title} link={item.link} />;
 
   return (
     <SafeAreaView>
-      <ScrollView>
-        <FlatList data={api} renderItem={renderItem} keyExtractor={(item) => item.id} />
-      </ScrollView>
+      <View style={styles.container}>
+        <FlatList data={api} renderItem={renderItem} keyExtractor={(item) => item.id.toString()} />
+      </View>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: { paddingBottom: 15, borderColor: 'red' },
+});
