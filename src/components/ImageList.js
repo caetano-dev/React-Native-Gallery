7310b4027b64732549e1aca9156c7e4ee3252c27
drull1000 <pcaetanop@protonmail.com>
2021-07-22 20:44:05 -0300

    API fetch

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import { SafeAreaView, FlatList, View, StyleSheet } from 'react-native';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, FlatList, View, StyleSheet, Text } from 'react-native';
 import { api } from '../data/Api';
 import { ImageItem } from './ImageItem';
 
 export function ImageList() {
+  const [images, setImages] = useState([]);
+  useEffect(() => {
+    /*TO-DO: fix the link and add the values to the flatlist*/
+    axios.get('http://127.0.0.1:8000/').then((res) => {
+      const data = res.data;
+      setImages(data.link);
+    });
+  }, []);
+
   const renderItem = ({ item }) => <ImageItem title={item.title} link={item.link} />;
 
   return (
     <SafeAreaView>
       <View style={styles.container}>
+        <Text style={styles.title}>{images}</Text>
         <FlatList data={api} renderItem={renderItem} keyExtractor={(item) => item.id.toString()} />
       </View>
     </SafeAreaView>
@@ -17,4 +28,5 @@ export function ImageList() {
 
 const styles = StyleSheet.create({
   container: { paddingBottom: 15, borderColor: 'red' },
+  title: { fontSize: 20, fontWeight: 'bold' },
 });
